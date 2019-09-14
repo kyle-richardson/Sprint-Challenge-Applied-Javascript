ec8deee5c318b3d8084c07f8c78eb2c54b70778a
Kyle Richardson <kyle.richardson1@gmail.com>
2019-09-13 21:37:55 -0500

    MVP and basic stretch done

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,54 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function Carousel() {
+  const topDiv = document.createElement('div')
+  const leftButton = topDiv.appendChild(document.createElement('div'))
+
+  //images in between
+  const image1 = topDiv.appendChild(document.createElement('img'))
+  image1.src = "./assets/carousel/mountains.jpeg"
+  image1.style.display = "block"
+
+  const image2 = topDiv.appendChild(document.createElement('img'))
+  image2.src = "./assets/carousel/computer.jpeg"
+
+  const image3 = topDiv.appendChild(document.createElement('img'))
+  image3.src = "./assets/carousel/trees.jpeg"
+
+  const image4 = topDiv.appendChild(document.createElement('img'))
+  image4.src = "./assets/carousel/turntable.jpeg"
+
+  const rightButton = topDiv.appendChild(document.createElement('div'))
+
+  topDiv.classList.add('carousel')
+  leftButton.classList.add('left-button')
+  rightButton.classList.add('right-button')
+
+  let currIndex = 0;
+  const photoArray = [image1, image2, image3, image4]
+
+  leftButton.addEventListener('click', e=> {
+    currIndex===3 ? currIndex = 0 : currIndex+=1
+    photoArray.forEach((photo,ind) => {
+      (ind === currIndex) 
+        ? photo.style.display = "block" 
+        : photo.style.display = "none"
+    })
+  })
+
+  rightButton.addEventListener('click', e=> {
+    currIndex===0 ? currIndex = 3 : currIndex-=1
+    photoArray.forEach((photo,ind) => {
+      (ind === currIndex) 
+        ? photo.style.display = "block" 
+        : photo.style.display = "none"
+    })
+  })
+
+  return topDiv
+}
+
+const insertCarousel = document.querySelector('.carousel-container')
+insertCarousel.appendChild(Carousel())
